02768bfd42e1c4e16d921ff42c269d32db00374d
R Memon <parvez.r.memon.accenture@gmail.com>
2025-08-05 18:33:39 +0530

    Styling as per Fima in Chat Popup

--- src/pages/MessageBubble.js
+++ src/pages/MessageBubble.js
@@ -10,8 +10,9 @@ const MessageBubble = ({ message }) => {
       sx={{
         p: 1,
         alignSelf: isUser ? "flex-end" : "flex-start",
-        backgroundColor: isUser ? "#e3f2fd" : "#f1f8e9",
+        backgroundColor: isUser ? "#1B6CFF" : "#e3f2fd",
         maxWidth: "80%",
+        color: isUser ? "#FFFFFF" : "#091521B8",
       }}
     >
       <Typography variant="body2">{message.text}</Typography>
